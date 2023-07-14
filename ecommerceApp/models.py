86b550c81ce580b439a8d342fbf078e2c6b6c0d6
MeMadDev <devanshkhandelwal930@gmail.com>
2023-07-14 12:01:08 +0530

    created contact page

--- ecommerceApp/models.py
+++ ecommerceApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    desc=models.TextField(max_length=100)
+    phone_no=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
--- ecommerceApp/views.py
+++ ecommerceApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from ecommerceApp.models import Contact
+from django.contrib import messages
 
 def index(request):
     return render(request,"index.html")
@@ -13,6 +15,15 @@ def about(request):
     return render(request,"about.html")
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get("full_name")
+        email=request.POST.get("email")
+        desc=request.POST.get("description")
+        phone_no=request.POST.get("phone_no" )
+        my_query=Contact(name=name,email=email,desc=desc,phone_no=phone_no)
+        my_query.save()
+        messages.info(request,"We will get back to you soon....")
+        return render(request,"contact.html")
     return render(request,"contact.html")
 
 def cart(request):
